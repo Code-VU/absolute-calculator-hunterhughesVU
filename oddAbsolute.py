0fd66c7164e68d10945bd602241f1682151ba532
Hunter Hughes <Hunter.Hughes@vu.com>
2022-02-18 11:57:06 -0600

    Initial Commit

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,10 +2,17 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+
+    result = abs(21 - int(in_num))
+
+    if(int(in_num) > 21):
+        result = result*2
+
+    print("Result:",result)
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculateAbsolute()
+#calculateAbsolute()
